85b8c535a381539417c551bb7d9645ff060cffa6
Kelcey Wilson <kelceywilson@gmail.com>
2017-10-16 14:26:08 -0700

    Show a weekday & tests complete

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,19 +1,13 @@
-// 20: Git history shows frequent commits.
-// 10: package.json is provided when your code has package dependencies.
-// 10: Your tests for part 1 can be run with the command $ npm test.
-
-// Show a weekday
-//  10: A test using expected/valid inputs for the weekday() function is written in tests.js.
-//  10: A test using unexpected/invalid inputs for the weekday() function is written in tests.js.
-//  30: Correct implementation of the weekday() is defined in functions.js.
-//  10: Tests for weekday() are passing.
-
+/** *************************
+      Show a weekday
+************************** */
+// Implementation: weekday function takes date object as input
+// Example: weekday(new Date(2017, 5, 19));  => returns "Mon"
 function weekday(date) {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const day = days[date.getDay()];
   return day;
 }
 
-// const d = new Date(1969, 8, 23); // June 19, 2017
-
-// console.log(weekday(d));
+// console.log(weekday('blah'));
+module.exports = weekday;
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,30 +1,5 @@
-10: A test using expected/valid inputs for the weekday() function is written in tests.js.
-10: A test using unexpected/invalid inputs for the weekday() function is written in tests.js.
-
-
 const { expect } = require('chai');
-const { exec } = require('child_process');
-// these requires require fileio.js, which causes the taskFile to be created
-const add = require('./commands/add');
-const list = require('./commands/list');
-const del = require('./commands/delete');
-const comp = require('./commands/complete');
-
-const {
-  readFromFile,
-  writeToFile,
-  taskFile,
-} = require('./fileio');
-
-// bds: also: you should be resetting the test file *every* test, and then
-// bds: setting up only what you need for the test in that file.
-// bds: Tests should not rely on previous tests to populate the test file.
-// bds: What if the test populating the test file fails? Then the following test
-// bds: would fail as well. Tests need to be independent.
-// bds: Make heavy use of before() and beforeEach()
-
-
-// run tests by running npm test on the command line
+const weekday = require('./functions');
 
 // sanity check
 describe('Mocha sanity test', () => {
@@ -33,115 +8,27 @@ describe('Mocha sanity test', () => {
   });
 });
 
-
-// bds: this section's great :-)
-// test suite covering missing or invalid commands
-describe('error message for missing or invalid commands', () => {
-  // test specs (unit tests)
-  it('displays error msg for missing command', (done) => {
-    exec('./tasks', (err, stdout, stderr) => {
-      expect(stdout).to.include('Missing command');
-      // done has to run before it can move on to next test
-      done();
-    });
-  });
-  it('displays error for invalid command', (done) => {
-    exec('./tasks blah', (err, stdout, stderr) => {
-      expect(stdout).to.include('Invalid command');
-      done();
-    });
-  });
-});
-
-
-// bds: more tests! Does it give the task the right task number?
-// bds: does the task file have the right number of tasks if you add
-// bds: when it's empty? Does it have the right number of tasks if you
-// bds: add when it has a task? Does the task get the right task number
-// bds: if you add after having deleted? After having completed?
-
-// test suite covering functions
-describe('add()', () => {
+// test suite covering function
+describe('weekday()', () => {
   it('is a function', () => {
-    expect(add).to.be.a('function');
+    expect(weekday).to.be.a('function');
   });
-  it('displays success message when task added', (done) => {
-    exec('./tasks add add test', (err, stdout, stderr) => {
-      expect(stdout).to.include('Created task');
-      done();
+  // Req: test using expected/valid inputs for the weekday()
+  context('returns expected output for valid inputs', () => {
+    it('weekday(new Date(2017, 5, 19)) should equal Mon', () => {
+      expect(weekday(new Date(2017, 5, 19))).to.equal('Mon');
+    });
+    it('weekday(new Date(2017, 6, 19)) should equal Wed', () => {
+      expect(weekday(new Date(2017, 6, 19))).to.equal('Wed');
     });
   });
-  it('displays error when "add" is not followed by a task', (done) => {
-    exec('./tasks add', (err, stdout, stderr) => {
-      expect(stdout).to.include('Missing task');
-      done();
+  // Req: test using unexpected/invalid inputs for the weekday()
+  context('returns expected output for valid inputs', () => {
+    it('weekday("blah") to throw a TypeError', () => {
+      expect(() => { weekday('blah'); }).to.throw(TypeError);
     });
-  });
-});
-
-// bds: more tests! What if the list is empty? Does it do the right thing after
-// bds: deleting? After completing? And here, you're not testing
-// bds: whether the output actually contains the expected tasks.
-describe('list()', () => {
-  it('is a function', () => {
-    expect(list).to.be.a('function');
-  });
-  it('displays task list', (done) => {
-    exec('./tasks list', (err, stdout, stderr) => {
-      expect(stdout).to.include('ID Description');
-      done();
-    });
-  });
-});
-
-// bds: what other tests could you do here?
-describe('del()', () => {
-  let taskList;
-  beforeEach((done) => {
-    taskList = readFromFile();
-    done();
-  });
-  it('is a function', () => {
-    expect(del).to.be.a('function');
-  });
-  it('displays success message when task deleted', (done) => {
-    exec(`./tasks delete ${taskList.tasks.length}`, (err, stdout, stderr) => {
-      expect(stdout).to.include('Deleted task');
-      done();
-    });
-  });
-  it('displays an error if the task id entered does not exist', (done) => {
-    exec(`./tasks delete ${taskList.tasks.length}`, (err, stdout, stderr) => {
-      expect(stdout).to.include('No task by that number exists');
-      done();
-    });
-  });
-});
-
-// bds: what other tests could you do here?
-describe('comp()', () => {
-  let taskList;
-  before((done) => {
-    exec('./tasks add comp test', (err, stdout, stderr) => { done(); });
-  });
-  it('is a function', () => {
-    expect(comp).to.be.a('function');
-  });
-  beforeEach((done) => {
-    taskList = readFromFile();
-    done();
-  });
-  it('displays success message when task completed', (done) => {
-    console.log(taskList.tasks.length);
-    exec('./tasks complete 23', (err, stdout, stderr) => {
-      expect(stdout).to.include('Completed task');
-      done();
-    });
-  });
-  it('displays an error if the task id entered does not exist', (done) => {
-    exec('./tasks complete 0', (err, stdout, stderr) => {
-      expect(stdout).to.include('No task by that number exists');
-      done();
+    it('weekday() to throw a TypeError', () => {
+      expect(() => { weekday(); }).to.throw(TypeError);
     });
   });
 });

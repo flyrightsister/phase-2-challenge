6986c9a7ec1ea996ebda1f3d901c8796c57e7936
Kelcey Wilson <kelceywilson@gmail.com>
2017-10-17 11:02:28 -0700

    Part 1 complete

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,13 +1,72 @@
 /** *************************
       Show a weekday
 ************************** */
-// Implementation: weekday function takes date object as input
-// Example: weekday(new Date(2017, 5, 19));  => returns "Mon"
+// Implementation: weekday(date) takes date object as input
+// Example: weekday(new Date(2017, 5, 19));  // => "Mon"
 function weekday(date) {
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const day = days[date.getDay()];
   return day;
 }
 
-// console.log(weekday('blah'));
-module.exports = weekday;
+/** *************************
+      Get a snippet
+************************** */
+/* Implementation: snippet(string, maxlength) takes a string and maxlength as arguments. It shortens the string to the maxlength and adds the ellipsis character ("…") to the end of string, and then returns the shortened string. If the string is shorter than the maxlength, just return the string unmodified.
+Example: snippet("For the following exercises,", 10); // => "For the fo…" */
+function snippet(string, maxlength) {
+  if (typeof string !== 'string') {
+    return 'First argument must be a string';
+  }
+  if (typeof maxlength !== 'number') {
+    return 'Second argument must be a number';
+  }
+  if (string.length > maxlength) {
+    return `${string.slice(0, maxlength)}\u2026`;
+  }
+  return string;
+}
+
+/** *************************
+    Number of properties
+************************** */
+/* Implementation: numProps(obj) takes and object as an argument and returns the number of its non-inherited properties.
+Example: numProps({name: 'Dominique'}); // => 1 */
+function numProps(obj) {
+  if (typeof obj === 'object' && obj !== null && !Array.isArray(obj)) {
+    return Object.keys(obj).length;
+  }
+  return 'Argument must be an object, not null, and not and array';
+}
+
+/** *************************
+    Filter between
+************************** */
+/* Implementation: filterBetween(array, min, max) takes an array of numbers, a min value, and a max value. It returns a new array containing only the elements that are greater than or equal to min and less than or equal to max.
+Example: filterBetween([5, 15, 25, 30, 35], 15, 34) // => [15, 25, 30] */
+
+function filterBetween(array, min, max) {
+  const filteredArray = [];
+  if (!Array.isArray(array) || array === undefined) {
+    return 'First argument is required and must be an array.';
+  }
+  if (!Number.isFinite(min) || !Number.isFinite(max) || array === undefined) {
+    return 'Second and third argument are required and must be a number.';
+  }
+  array.forEach((number) => {
+    if (number >= min && number <= max) {
+      filteredArray.push(number);
+    }
+  });
+  return filteredArray;
+}
+
+// console.log(filterBetween([5, 15, 25, 30, 35], 15, 34));
+// console.log(filterBetween(array, min, max));
+
+module.exports = {
+  weekday,
+  snippet,
+  numProps,
+  filterBetween,
+};
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
-const weekday = require('./functions');
+const {
+  weekday, snippet, numProps, filterBetween,
+} = require('./functions');
 
 // sanity check
 describe('Mocha sanity test', () => {
@@ -8,12 +10,13 @@ describe('Mocha sanity test', () => {
   });
 });
 
-// test suite covering function
-describe('weekday()', () => {
+/** *************************
+  Tests for weekday(date)
+************************** */
+describe('weekday(date)', () => {
   it('is a function', () => {
     expect(weekday).to.be.a('function');
   });
-  // Req: test using expected/valid inputs for the weekday()
   context('returns expected output for valid inputs', () => {
     it('weekday(new Date(2017, 5, 19)) should equal Mon', () => {
       expect(weekday(new Date(2017, 5, 19))).to.equal('Mon');
@@ -22,13 +25,115 @@ describe('weekday()', () => {
       expect(weekday(new Date(2017, 6, 19))).to.equal('Wed');
     });
   });
-  // Req: test using unexpected/invalid inputs for the weekday()
-  context('returns expected output for valid inputs', () => {
+  context('returns errors for invalid inputs', () => {
     it('weekday("blah") to throw a TypeError', () => {
       expect(() => { weekday('blah'); }).to.throw(TypeError);
     });
     it('weekday() to throw a TypeError', () => {
       expect(() => { weekday(); }).to.throw(TypeError);
+    });
+  });
+});
+
+/** ************************************
+  Tests for snippet(string, maxlength)
+************************************* */
+describe('snippet(string, maxlength)', () => {
+  it('is a function', () => {
+    expect(snippet).to.be.a('function');
+  });
+  context('returns expected output for valid inputs', () => {
+    it('snippet("For the following exercises,", 10) should return "For the fo…"', () => {
+      expect(snippet('For the following exercises,', 10)).to.equal('For the fo…');
+    });
+    it('snippet("Hello, world!", 20) to equal "Hello, world!"', () => {
+      expect(snippet('Hello, world!', 20)).to.equal('Hello, world!');
+    });
+  });
+  context('returns errors for invalid inputs', () => {
+    it('snippet(blah, 10) should return a ReferenceError', () => {
+      expect(() => { snippet(blah, 10); }).to.throw(ReferenceError);
+    });
+    it('snippet(10, 10) should return "First argument must be a string"', () => {
+      expect(snippet(10, 10)).to.equal('First argument must be a string');
+    });
+    it('snippet(10, "blah") should return "Second argument must be a number"', () => {
+      expect(snippet('hi', 'blah')).to.equal('Second argument must be a number');
+    });
+  });
+});
+
+/** *************************
+  Tests for numProps(obj)
+************************** */
+const friend = {
+  name: 'Dominique',
+  age: 30,
+  phone: '555-555-5555',
+};
+
+describe('numProps(obj)', () => {
+  it('is a function', () => {
+    expect(numProps).to.be.a('function');
+  });
+  context('returns expected output for valid inputs', () => {
+    it('numProps({name: "joe", age: 30}) should return 2', () => {
+      expect(numProps({ name: 'joe', age: 30 })).to.equal(2);
+    });
+    it('numProps({}) should return 0', () => {
+      expect(numProps({})).to.equal(0);
+    });
+  });
+  context('returns errors for invalid inputs', () => {
+    it('numProps(["joe", 30]) should return a usage error', () => {
+      expect(numProps(['joe', 30])).to.equal('Argument must be an object, not null, and not and array');
+    });
+    it('numProps(0) should return a usage error', () => {
+      expect(numProps(0)).to.equal('Argument must be an object, not null, and not and array');
+    });
+    it('numProps(true) should return a usage error', () => {
+      expect(numProps(true)).to.equal('Argument must be an object, not null, and not and array');
+    });
+    it('numProps("Joe") should return a usage error', () => {
+      expect(numProps('joe')).to.equal('Argument must be an object, not null, and not and array');
+    });
+    it('numProps(undefined) should return a usage error', () => {
+      expect(numProps(undefined)).to.equal('Argument must be an object, not null, and not and array');
+    });
+  });
+});
+
+/** ***************************************
+  Tests for filterBetween(array, min, max)
+**************************************** */
+
+describe('filterBetween(array, min, max)', () => {
+  it('is a function', () => {
+    expect(filterBetween).to.be.a('function');
+  });
+  context('returns expected output for valid inputs', () => {
+    it('filterBetween([5, 15, 25, 30, 35], 15, 34) should return an array', () => {
+      expect(filterBetween([5, 15, 25, 30, 35], 15, 34)).to.be.an.instanceof(Array);
+    });
+    it('filterBetween([5, 15, 25, 30, 35], 15, 34) should not include 5, 35', () => {
+      expect(filterBetween([5, 15, 25, 30, 35], 15, 34)).to.not.include(5, 35);
+    });
+    it('filterBetween([ 15, 25, 30 ], 0, 0) should return []', () => {
+      expect(filterBetween([15, 25, 30], 50, 0)).to.be.empty;
+    });
+  });
+  context('returns errors for invalid inputs', () => {
+    it('filterBetween("array", "min", "max") should return a usage error', () => {
+      expect(filterBetween('array', 'min', 'max')).to.equal('First argument is required and must be an array.');
+    });
+    it('filterBetween([], "min", "max") should return a usage error', () => {
+      expect(filterBetween([], 'min', 'max')).to.equal('Second and third argument are required and must be a number.');
+    });
+    it('filterBetween([], 10, "max") should return a usage error', () => {
+      expect(filterBetween([], 10, 'max')).to.equal('Second and third argument are required and must be a number.');
+    });
+    it('filterBetween([], "min", 10) should return a usage error', () => {
+      expect(filterBetween([], 'min', 10)).to.equal('Second and third argument are required and must be a number.');
     });
   });
 });

33277291059d606bf080cd01fdc72df81bee3341
Kelcey Wilson <kelceywilson@gmail.com>
2017-10-17 14:25:38 -0700

    Part 2 more than mostly done

--- part-2/searchByState.js
+++ part-2/searchByState.js
@@ -4,31 +4,34 @@ const fs = require('fs');
 const clients = () => JSON.parse(fs.readFileSync('./clients.json', 'utf8'));
 
 // console.log(clients());
-const input = process.argv.slice(2).join(' ');
+const input = process.argv[2];
 
-function nameSearch(name) {
-  console.log(`Finding clients \x1b[31m%s\x1b[37m state "${name}"...\n`, 'in');
+function stateSearch(state) {
+  console.log(`Finding clients \x1b[31m%s\x1b[37m state "${state}"...\n`, 'in');
 
-  const nameSearchResults = [];
+  const stateSearchResults = [];
   const clientList = clients();
   clientList.forEach((client) => {
     // see if client.rep_name begins with name
-    const repName = client.rep_name.toLowerCase();
+    const stateName = client.state.toLowerCase();
 
-    for (let i = 0; i < name.length; i++) {
-      if (repName[i] !== name[i].toLowerCase()) {
+    for (let i = 0; i < state.length; i++) {
+      if (stateName[i] !== state[i].toLowerCase()) {
         break;
       }
-      if (i === name.length - 1) {
-        nameSearchResults.push({
+      if (i === state.length - 1) {
+        stateSearchResults.push({
           id: client.id,
           rep_name: client.rep_name,
+          company: client.company,
+          city: client.city,
+          state: client.state,
         });
       }
     }
   });
-  return nameSearchResults;
+  return stateSearchResults;
 }
 
-console.log(nameSearch(input));
-console.log('\x1b[36m%s\x1b[0m', 'I am cyan'); // cyan
+console.log(stateSearch(input));
+// console.log('\x1b[36m%s\x1b[0m', 'I am cyan'); // cyan
